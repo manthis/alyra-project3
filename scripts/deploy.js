9abbb0bfb25e60121d9e896de97055cd3b7e9ec7
Maxime AUBURTIN <maxime@auburt.in>
2024-01-22 12:52:39 +0100

    Added prettier configuration. Updated deploy script.

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,30 +4,19 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const hre = require("hardhat");
+const hre = require('hardhat');
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+    const voting = await hre.ethers.deployContract('Voting');
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+    await voting.waitForDeployment();
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+    console.log(`Voting deployed to ${voting.target}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
