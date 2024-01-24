96d791a9e0716c0f40b1420e443c9a0bdc8ace15
Maxime AUBURTIN <maxime@auburt.in>
2024-01-24 15:03:09 +0100

    Small fixes

--- frontend/components/interfaces/public/PublicSection.jsx
+++ frontend/components/interfaces/public/PublicSection.jsx
@@ -1,6 +1,6 @@
 const PublicSection = () => {
     return (
-        <div className="border-2 border-slate-600 rounded-lg w-2/4 flex flex-col justify-center items-center p-4">
+        <div className="border-2 border-slate-600 rounded-lg w-full flex flex-col justify-center items-center p-4">
             <h1 className="text-xl p-2">Voting Information</h1>
             <div>Vote step / Winner / Logs</div>
         </div>
