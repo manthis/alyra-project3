01aaa27ae1aff5798f65f5dda621417420be9887
Maxime AUBURTIN <maxime@auburt.in>
2024-01-24 15:29:23 +0100

    Added vote status in public section

---
+++ frontend/components/contract/WorkflowStatuses.js
@@ -0,0 +1,9 @@
+export const WorkflowStatus = {
+    0: 'Vote Not Started',
+    1: 'Registering Voters',
+    2: 'Proposals Registration Started',
+    3: 'Proposals Registration Ended',
+    4: 'Voting Session Started',
+    5: 'Voting Session Ended',
+    6: 'Votes Tallied',
+};
--- frontend/components/interfaces/public/PublicSection.jsx
+++ frontend/components/interfaces/public/PublicSection.jsx
@@ -1,7 +1,29 @@
+import { WorkflowStatus } from "@/components/contract/WorkflowStatuses";
+import { useContractRead } from "wagmi";
+import VotingABI from "../../contract/VotingABI";
+
 const PublicSection = () => {
+    const { data: workflowStatus, isSuccess } = useContractRead({
+        address: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+        abi: VotingABI,
+        functionName: "workflowStatus",
+    });
+
+    let workflowStatusText = "";
+    if (isSuccess) {
+        workflowStatusText = WorkflowStatus[workflowStatus];
+    }
+
     return (
         <div className="border-2 border-slate-600 rounded-lg w-full flex flex-col justify-center items-center p-4">
-            <h1 className="text-xl p-2">Voting Information</h1>
+            <h1 className="text-xl p-2 font-extrabold">Voting Information</h1>
+            <div className="bg-blue-500 rounded-full flex justify-center items-center py-2 px-4">
+                Workflow Status:{" "}
+                <span className="text-white ml-2 font-bold">
+                    {workflowStatusText}
+                </span>
+            </div>
+            <br />
             <div>Vote step / Winner / Logs</div>
         </div>
     );
