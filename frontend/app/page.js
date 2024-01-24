78a55f6a10e5d2685e35190333a0425f896415e7
Maxime AUBURTIN <maxime@auburt.in>
2024-01-24 12:44:21 +0100

    Added authentication. Need to add roles now.

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,14 +1,14 @@
 'use client';
 
-import { ConnectButton } from '@rainbow-me/rainbowkit';
+import { AuthProvider } from '@/components/auth/AuthContext';
+import Home from '@/components/Home';
 
-export default function Home() {
+const App = () => {
     return (
-        <main className='flex min-h-screen flex-col items-center justify-between p-24 font-mono'>
-            <div className='z-10 max-w-5xl w-full items-center justify-between text-sm lg:flex'>
-                <h1>Alyra - Project 3</h1>
-                <ConnectButton />
-            </div>
-        </main>
+        <AuthProvider>
+            <Home />
+        </AuthProvider>
     );
-}
+};
+
+export default App;
