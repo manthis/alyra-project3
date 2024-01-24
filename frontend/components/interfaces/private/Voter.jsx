53cf021b0b301780f46f43272732c58e38b54cd4
Maxime AUBURTIN <maxime@auburt.in>
2024-01-24 16:17:00 +0100

    Small fixes

--- frontend/components/interfaces/private/Voter.jsx
+++ frontend/components/interfaces/private/Voter.jsx
@@ -2,6 +2,17 @@ export default function Voter() {
     return (
         <div className="border-2 border-slate-600 rounded-lg w-full flex flex-col justify-center items-center p-4 mb-4">
             <h1>Voter section</h1>
+            {/**
+                TODO: 
+
+                Depending of the current step:
+                - step 0 -> nothing
+                - step 1 -> nothing
+                - step 2 -> a text area and a button to submit proposals
+                - step 3 -> nothing
+                - step 4 -> the list of proposition to be selected and a button to vote
+                - nothing for the other steps
+             */}
         </div>
     );
 }
