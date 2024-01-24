45a66ea2a24cb9fc18a4ea629a2355165cd9790e
Maxime AUBURTIN <maxime@auburt.in>
2024-01-24 21:16:14 +0100

    Updated Public Section with logs

--- frontend/components/contract/ContractService.js
+++ frontend/components/contract/ContractService.js
@@ -1,15 +1,12 @@
-/* TODO: build a service using Wagmi Core to call the contract functions */
-
-import { getPublicClient, prepareWriteContract, waitForTransaction } from '@wagmi/core';
-import { votingAbi } from './VotingAbi';
-
-const client = getPublicClient();
+import { getPublicClient, prepareWriteContract, waitForTransaction, writeContract } from '@wagmi/core';
+import { parseAbiItem } from 'viem';
+import votingABI from './VotingAbi';
 
 const writeToContract = async (_functionName, _args, _account) => {
     try {
         const { request } = await prepareWriteContract({
             address: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
-            abi: votingAbi,
+            abi: votingABI,
             functionName: _functionName,
             args: _args,
             account: _account,
@@ -27,4 +24,16 @@ export const addVoter = async (_voterAddr, _accountAddr) => {
     // TODO check _voterAddr is a valid address before calling contract
     await writeToContract('addVoter', [_voterAddr], _accountAddr);
     console.log(`Voter added: ${_voterAddr}`);
+};
+
+export const getVoterRegistrationLogs = async () => {
+    const client = getPublicClient();
+
+    const logs = await client.getLogs({
+        event: parseAbiItem('event VoterRegistered(address voterAddress)'),
+        fromBlock: 0n,
+        toBlock: 'latest',
+    });
+
+    return logs;
 };
