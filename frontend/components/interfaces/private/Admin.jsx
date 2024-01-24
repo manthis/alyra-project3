ed6e375720538108760e1cde1df3212ade406b09
Maxime AUBURTIN <maxime@auburt.in>
2024-01-24 15:29:23 +0100

    Added vote status in public section

--- frontend/components/interfaces/private/Admin.jsx
+++ frontend/components/interfaces/private/Admin.jsx
@@ -1,7 +1,7 @@
 export default function Admin() {
     return (
         <div className="border-2 border-slate-600 rounded-lg flex flex-col justify-center items-center p-4 mb-4 w-full">
-            Admin section
+            <h1>Admin section</h1>
         </div>
     );
 }
