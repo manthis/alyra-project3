a3af17a5febef1cd0c44d05d90be16cac63c0c01
Maxime AUBURTIN <maxime@auburt.in>
2024-01-24 13:15:29 +0100

    Updated authentication

---
+++ frontend/components/interfaces/Home.jsx
@@ -0,0 +1,28 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import PrivateSection from "./private/PrivateSection";
+import PublicSection from "./public/PublicSection";
+
+const Home = () => {
+    return (
+        <>
+            <div
+                id="header"
+                className="fixed top-0 h-16 bg-transparent w-full flex justify-between items-center border-b-2 border-slate-600"
+            >
+                <h1 className="text-xl font-bold pl-4">
+                    Alyra: Project 3 - Voting dApp
+                </h1>
+                <div className="m-2">
+                    {/* RainbowKit Button */}
+                    <ConnectButton />
+                </div>
+            </div>
+            <section className="min-h-screen bg-slate-200 mt-16 flex flex-col justify-top items-center">
+                <PrivateSection />
+                <PublicSection />
+            </section>
+        </>
+    );
+};
+
+export default Home;
---
+++ frontend/components/interfaces/private/PrivateSection.jsx
@@ -0,0 +1,17 @@
+import { useAuth } from "../../auth/AuthContext";
+import withAuthentication from "../../auth/withAuthentication";
+import MessageBox from "../MessageBox";
+
+const PrivateSection = () => {
+    const user = useAuth();
+
+    return (
+        <div className="border-2 border-slate-600 rounded-lg m-2 w-2/4 flex flex-col justify-center items-center p-4">
+            <p>Private Section: under construction</p>
+            <p>Admin section</p>
+            <p>Voter section</p>
+        </div>
+    );
+};
+
+export default withAuthentication(PrivateSection, MessageBox);
