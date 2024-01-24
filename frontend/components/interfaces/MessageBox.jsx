f50c42c6a66e191c3b9a75adf2bb129e5e0dee68
Maxime AUBURTIN <maxime@auburt.in>
2024-01-24 15:04:37 +0100

    Small fixes

--- frontend/components/interfaces/MessageBox.jsx
+++ frontend/components/interfaces/MessageBox.jsx
@@ -1,6 +1,6 @@
 const MessageBox = ({ message }) => {
     return (
-        <div className="bg-orange-400 w-2/4 flex flex-col justify-center items-center border-2 border-slate-600 rounded-lg mb-2">
+        <div className="bg-orange-400 w-full flex flex-col justify-center items-center border-2 border-slate-600 rounded-lg mb-2">
             <p className="p-4">{message}</p>
         </div>
     );
