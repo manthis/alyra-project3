1676eeafb9cf5fc561a2e4479f9e3d840ae703d0
Maxime AUBURTIN <maxime@auburt.in>
2024-01-25 18:13:05 +0100

    Step2 ready before implementing Voters action.

---
+++ frontend/components/interfaces/private/admin/StepWithNothing.jsx
@@ -0,0 +1,11 @@
+const StepWithNothing = () => {
+    return (
+        <div className="flex justify-center items-center flex-col w-4/5">
+            <p className="mb-2 italic">
+                Nothing to do as an admin at this vote step.
+            </p>
+        </div>
+    );
+};
+
+export default StepWithNothing;
