2defa5a28b4a4545d6aeff026c2d6685a4a6845f
Maxime AUBURTIN <maxime@auburt.in>
2024-01-25 18:21:06 +0100

    First step is done.

---
+++ frontend/components/interfaces/private/StepWithNothing.jsx
@@ -0,0 +1,9 @@
+const StepWithNothing = ({ message }) => {
+    return (
+        <div className="flex justify-center items-center flex-col w-4/5">
+            <p className="mb-2 italic">{message}</p>
+        </div>
+    );
+};
+
+export default StepWithNothing;
