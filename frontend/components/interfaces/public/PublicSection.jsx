5e62d8604add488f33185169d492fb2c5a89e3e6
Maxime AUBURTIN <maxime@auburt.in>
2024-01-25 23:01:11 +0100

    Updated global UI

--- frontend/components/interfaces/public/PublicSection.jsx
+++ frontend/components/interfaces/public/PublicSection.jsx
@@ -4,8 +4,8 @@ import WorkflowStatusWatcher from "./WorkflowStatusWatcher";
 const PublicSection = () => {
     return (
         <>
-            <div className="border-2 border-slate-700 rounded-lg w-full flex flex-col justify-center items-center p-4 mb-4 bg-slate-600">
-                <h1 className="text-xl font-bold m-4">Voting Information</h1>
+            <div className="border-2 border-slate-700 rounded-full flex flex-col justify-center items-center p-4 bg-fuchsia-700 w-[400px] h-[400px] mb-20">
+                <h1 className="text-xl font-bold m-4">Vote Status</h1>
 
                 <WorkflowStatusWatcher />
                 <WinnerWatcher />
