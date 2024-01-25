ae28d029d370b0bcfe9ed324d1ed440d645fa3df
Maxime AUBURTIN <maxime@auburt.in>
2024-01-25 21:04:25 +0100

    Modified import of Openzeppelin contract

--- backend/contracts/Voting.sol
+++ backend/contracts/Voting.sol
@@ -2,7 +2,7 @@
 
 pragma solidity 0.8.20;
 
-import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 
 contract Voting is Ownable {
