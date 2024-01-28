77fe4184f6af232a0c3cb211d9b63040dcee1302
Maxime AUBURTIN <maxime@auburt.in>
2024-01-28 16:57:01 +0100

    Emptying input boxes on success

--- frontend/components/interfaces/private/admin/RegisteringVoters.jsx
+++ frontend/components/interfaces/private/admin/RegisteringVoters.jsx
@@ -1,16 +1,21 @@
 import { useAuthContext } from "@/components/contexts/AuthContext";
 import { addVoter } from "@/components/contract/ContractService";
+import { useRef } from "react";
 
 const Step1 = ({ _errorCallback, _infoCallback }) => {
     const user = useAuthContext();
+    const inputRef = useRef(null);
 
-    const RegisteringVoters = async (formData) => {
-        const address = formData.get("address");
+    const RegisteringVoters = async (event) => {
+        event.preventDefault();
+        const address = inputRef.current.value;
+
         try {
             await addVoter(address, user?.data.address);
             _infoCallback(
                 `Voter successfully registered with address: ${address}`
             );
+            inputRef.current.value = "";
         } catch (error) {
             _errorCallback(error);
         }
@@ -21,10 +26,11 @@ const Step1 = ({ _errorCallback, _infoCallback }) => {
                 Please register a voter using their ETH address:
             </p>
             <form
-                action={RegisteringVoters}
+                onSubmit={RegisteringVoters}
                 className="w-full flex justify-between"
             >
                 <input
+                    ref={inputRef}
                     className="border-1 border-slate-400 rounded-xl py-2 px-4 w-full mr-4 text-black"
                     type="text"
                     placeholder="0x0"
