4bc1cb59edc08119cf54513e41ad00767b085bbb
Maxime AUBURTIN <maxime@auburt.in>
2024-01-29 00:34:23 +0100

    Last fix

--- frontend/components/interfaces/MessageBox.jsx
+++ frontend/components/interfaces/MessageBox.jsx
@@ -1,10 +1,10 @@
-const MessageBox = ({ message, color = "bg-blue-600" }) => {
+const MessageBox = ({ message, color = "bg-red-700" }) => {
     let style =
         color +
         " text-white font-semibold flex flex-col justify-center items-center mb-4 fixed z-50";
 
-    if (color === "bg-blue-600") {
-        style += " bottom-40 italic animate-bounce rounded-full";
+    if (color === "bg-red-700") {
+        style += " italic animate-pulse rounded-full";
     } else {
         style += " top-40 w-2/5 rounded-lg max-w-[1000px] opacity-90";
     }
