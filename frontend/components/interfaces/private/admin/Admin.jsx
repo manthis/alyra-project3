d5c6f23552e9dfd9bbbf7b1b44686e3f89878981
Maxime AUBURTIN <maxime@auburt.in>
2024-01-28 23:09:36 +0100

    Added check to ensure there is at least one proposition before starting vote session.

--- frontend/components/interfaces/private/admin/Admin.jsx
+++ frontend/components/interfaces/private/admin/Admin.jsx
@@ -1,9 +1,8 @@
 import { useAuthContext } from "@/components/contexts/AuthContext";
 import { useContractContext } from "@/components/contexts/ContractContext";
 import {
-    endVotingSessionAndTallyVotes,
+    getOneProposal,
     startProposalsRegistering,
-    startVotingSession,
 } from "@/components/contract/ContractService";
 import { WorkflowStatus } from "@/components/contract/WorkflowStatuses";
 import { useEffect, useState } from "react";
@@ -42,7 +41,7 @@ export default function Admin({
                 }" to "${WorkflowStatus[workflowStep + 1]}"`
             );
         } catch (error) {
-            _errorCallback(error.message);
+            _errorCallback(error);
         }
     };
 
@@ -56,7 +55,21 @@ export default function Admin({
                 _infoCallback
             );
         } else if (workflowStep === 1) {
-            moveForward(startVotingSession, _errorCallback, _infoCallback);
+            try {
+                await getOneProposal(1, user?.data.address);
+                moveForward(startVotingSession, _errorCallback, _infoCallback);
+            } catch (error) {
+                if (error.message.includes("Array index is out of bounds")) {
+                    console.log("YALA");
+                    _errorCallback(
+                        new Error(
+                            "You must at least add one proposal before starting vote session!"
+                        )
+                    );
+                } else {
+                    _errorCallback(error);
+                }
+            }
         } else if (workflowStep === 3) {
             moveForward(
                 endVotingSessionAndTallyVotes,
@@ -68,25 +81,17 @@ export default function Admin({
         }
     };
 
-    const mustDisplayStepWithNothing =
-        contractContext.workflowStatus === 1 ||
-        contractContext.workflowStatus === 2 ||
-        contractContext.workflowStatus === 3;
-
     return (
         <div className="border-2 border-emerald-600 rounded-lg flex flex-col justify-center items-center p-4 mb-4 w-full bg-emerald-500">
             <h1 className="text-xl font-bold m-4">Admin section</h1>
 
             {/* If the step is 'Registering voters' */}
-            {contractContext.workflowStatus === 0 && (
+            {contractContext.workflowStatus === 0 ? (
                 <Step1
                     _errorCallback={_errorCallback}
                     _infoCallback={_infoCallback}
                 />
-            )}
-
-            {/* If the step doesn't require Admin involvment */}
-            {mustDisplayStepWithNothing && (
+            ) : (
                 <StepWithNothing message="Nothing to do as an Admin at this vote step" />
             )}
 
